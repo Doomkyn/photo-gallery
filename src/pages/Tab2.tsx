f18f7f6af1fec0c120c2ec25db5b3eb0fbec014d
Kevin <bonazzi.kevin@gmail.com>
2020-04-15 09:51:17 +0200

    commit

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,7 +7,6 @@ import {
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
-
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
