5e4558abea0ac639961bbacb20f964f92c8f9dc4
Kevin <bonazzi.kevin@gmail.com>
2020-04-15 09:51:50 +0200

    Edit to tabs

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -7,6 +7,7 @@ import {
 } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import { usePhotoGallery } from '../hooks/usePhotoGallery';
+
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
